1f97353ba575b6fe64b77b7f79d7f6d8f200e146
Elina Sotska <elinasockaya1203@gmail.com>
2021-05-19 14:31:38 +0200

    Added a search engine

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Hello World</p>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <h1>Dictionary</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Open-sourse code by Elina Sotska</small>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
