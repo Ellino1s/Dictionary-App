7312b3bc3c816f84dfe1e42214977f43c9c796ee
Elina Sotska <elinasockaya1203@gmail.com>
2021-05-26 15:52:45 +0200

    Styled heading

--- src/App.js
+++ src/App.js
@@ -5,9 +5,6 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">
-          <h1>Dictionary</h1>
-        </header>
         <main>
           <Dictionary />
         </main>
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import Results from "./Results";
+import Result from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
@@ -27,10 +27,33 @@ export default function Dictionary() {
   }
   return (
     <div className="Dictionary">
-      <form onSubmit={search}>
-        <input type="search" onChange={handleKeywordChange} />
-      </form>
-      <Results results={results} />
+      <header className="App-header">
+        <h1>Dictionary</h1>
+        <form
+          className="form-inline d-flex justify-content-center md-form form-sm"
+          onSubmit={search}
+        >
+          <input
+            className="form-control form-control-sm mr-2 w-50"
+            id="search-form"
+            type="search"
+            placeholder="What word wood you like to search?"
+            onChange={handleKeywordChange}
+          />
+          <button type="submit" id="search_icon">
+            <i className="fas fa-search"></i>
+          </button>
+        </form>
+        <hr />
+        <svg viewBox="0 0 1440 319">
+          <path
+            fill="#fff"
+            fill-opacity="1"
+            d="M0,32L48,80C96,128,192,224,288,224C384,224,480,128,576,90.7C672,53,768,75,864,96C960,117,1056,139,1152,149.3C1248,160,1344,160,1392,160L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+          ></path>
+        </svg>
+      </header>
+      <Result results={results} />
     </div>
   );
 }
